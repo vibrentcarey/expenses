2800a3a97282137ff93ce2af92b5c90b65752b18
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-10-30 20:02:58 -0700

    Add onChange and onSubmit functions to form

--- src/components/new-expense-components/NewExpenseForm.js
+++ src/components/new-expense-components/NewExpenseForm.js
@@ -1,27 +1,52 @@
+import { useState } from "react"
 import Card from "../wrapper-components/Card"
 import "./NewExpenseForm.css"
 
 const NewExpenseForm = () => {
+  const [expenseData, setExpenseData] = useState({
+    title: '',
+    amount: '',
+    date: ''
+  })
+
+  const setUserData = (e) => {
+    setExpenseData(prevExpenseData => {
+      return { ...prevExpenseData, [e.target.id]: e.target.value }
+    })
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+
+    const userExpenseData = {
+      title: expenseData.title,
+      amount: expenseData.amount,
+      date: expenseData.date
+    }
+
+    console.log(userExpenseData)
+  }
+
   return (
     <Card className="new-expense">
-      <form>
-      <h1>Please Enter An Expense Below</h1>
-      <hr className="expenses__line"/>
+      <form onSubmit={submitForm}>
+        <h1>Please Enter An Expense Below</h1>
+        <hr className="expenses__line" />
         <section className="new-expense__inputs">
           {/* Expense Title */}
           <article className="new-expense__input">
-            <label className="new-expense__input--type" for="expense-title">Title</label>
-            <input className="new-expense__input--input" id="expense-title" placeholder=" Gym Membership" />
+            <label className="new-expense__input--type" for="title">Title</label>
+            <input onChange={setUserData} className="new-expense__input--input" id="title" placeholder=" Gym Membership" />
           </article>
           {/* Expense Amount */}
           <article className="new-expense__input">
-            <label className="new-expense__input--type" for="expense-amount">Amount</label>
-            <input className="new-expense__input--input" id="expense-amount" placeholder=" 100" />
+            <label className="new-expense__input--type" for="amount">Amount</label>
+            <input onChange={setUserData} className="new-expense__input--input" id="amount" placeholder=" 100" />
           </article>
           {/* Expense Date */}
           <article className="new-expense__input">
-            <label className="new-expense__input--type" for="expense-date">Date</label>
-            <input className="new-expense__input--input" id="expense-date" placeholder=" dd.mm.yyyy" type='date' min="2021-10-01" max="2022-12-31" />
+            <label className="new-expense__input--type" for="date">Date</label>
+            <input onChange={setUserData} className="new-expense__input--input" id="date" placeholder=" dd.mm.yyyy" type='date' min="2021-10-01" max="2022-12-31" />
           </article>
         </section>
         <section className="new-expense__actions">
