29b98c2498e3954647b384cba3bddef9677728c4
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-10-31 11:00:35 -0700

    Add form validation

--- src/components/new-expense-components/NewExpenseForm.js
+++ src/components/new-expense-components/NewExpenseForm.js
@@ -8,6 +8,8 @@ const NewExpenseForm = ({ onFormSubmit }) => {
     amount: '',
     date: ''
   })
+  const [formIsValid, setFormIsValid] = useState(true)
+
   // Set The Values From The Form Into State
   const setUserData = (e) => {
     setExpenseData(prevExpenseData => {
@@ -18,6 +20,15 @@ const NewExpenseForm = ({ onFormSubmit }) => {
 
   const submitForm = (e) => {
     e.preventDefault()
+    // Check If Form Inputs Are Valid
+    if (!expenseData.title || !expenseData.amount || !expenseData.date) {
+      setFormIsValid(false)
+      setTimeout(() => {
+        setFormIsValid(true)
+      }, 3000)
+      return
+    }
+
     // Reformat The Date Into Individual Parts
     const dayOfExpense = expenseData.date.slice(-2)
     const monthOfExpense = expenseData.date.slice(5, 7)
@@ -27,7 +38,7 @@ const NewExpenseForm = ({ onFormSubmit }) => {
       id: Math.random().toString(),
       title: expenseData.title,
       amount: expenseData.amount,
-      date: new Date(yearOfExpense, parseInt(monthOfExpense) -1, dayOfExpense)
+      date: new Date(yearOfExpense, parseInt(monthOfExpense) - 1, dayOfExpense)
     }
 
     onFormSubmit(userExpenseData)
@@ -47,7 +58,7 @@ const NewExpenseForm = ({ onFormSubmit }) => {
           {/* Expense Amount */}
           <article className="new-expense__input">
             <label className="new-expense__input--type" for="amount">Amount</label>
-            <input onChange={setUserData} className="new-expense__input--input" id="amount" placeholder=" 100" />
+            <input onChange={setUserData} className="new-expense__input--input" id="amount" placeholder=" 100" type='number' min='0'/>
           </article>
           {/* Expense Date */}
           <article className="new-expense__input">
@@ -55,7 +66,7 @@ const NewExpenseForm = ({ onFormSubmit }) => {
             <input onChange={setUserData} className="new-expense__input--input" id="date" placeholder=" dd.mm.yyyy" type='date' min="2021-10-01" max="2022-12-31" />
           </article>
         </section>
-        :TODO: Extract data from Form into parent
+        {!formIsValid && <p className="new-expense__error">Please Fill Out All Form Fields</p>}
         <section className="new-expense__actions">
           <button className="new-expense__actions--button">Add Expense</button>
         </section>
