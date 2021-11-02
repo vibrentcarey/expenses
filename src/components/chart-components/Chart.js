8871c96d1bec41ee0e4734907e96515163a24485
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-11-01 22:13:32 -0700

    Add bar fill styling to chart

--- src/components/chart-components/Chart.js
+++ src/components/chart-components/Chart.js
@@ -3,6 +3,7 @@ import "./Chart.css"
 import ChartBar from "./ChartBar"
 
 const Chart = ({ expenses }) => {
+
   const chartInfo = [
     { label: 'Jan', value: 0 },
     { label: 'Feb', value: 0 },
@@ -17,8 +18,21 @@ const Chart = ({ expenses }) => {
     { label: 'Nov', value: 0 },
     { label: 'Dec', value: 0 },
   ]
+
+  // Update Each Value Based On The Month
+  for (let expense of expenses) {
+    const month = expense.date.getMonth()
+    chartInfo[month].value += parseInt(expense.amount)
+  }
+
+  // Convert Expense Objects To Number Values
+  const expenseValues = expenses.map(expense => parseInt(expense.amount))
+  const maxHeight = Math.max(...expenseValues)
+
+  console.log(` max ${maxHeight}`)
+
   const chartBars = chartInfo.map(monthlyData => {
-    return <ChartBar key={monthlyData.label} label={monthlyData.label} />
+    return <ChartBar key={monthlyData.label} label={monthlyData.label} height={monthlyData.value} maxHeight={maxHeight} />
   })
 
   return (
--- src/components/chart-components/ChartBar.js
+++ src/components/chart-components/ChartBar.js
@@ -1,13 +1,18 @@
 import "./ChartBar.css"
 
-const ChartBar = ({ label }) => {
-
+const ChartBar = ({ label, height, maxHeight }) => {
+  let barHeight = "0%"
+  // Calculate The Fill Percentage
+  if (maxHeight > 0 ) {
+    barHeight = (Math.round((height / maxHeight) * 100)) + "%"
+  }
+  console.log(barHeight)
   return (
     <div className="chart-bar">
-        <h2>{label}</h2>
-        <div className="chart-bar__inner">
-          <div className="chart-bar__fill"></div>
-        </div>
+      <h2>{label}</h2>
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ height: barHeight }}></div>
+      </div>
     </div>
   )
 }
